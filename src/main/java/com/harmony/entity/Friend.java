43c9e84003a9fd4dd8abfb8ac281991088aabdb2
azin1129 <nuestiloveyou@gmail.com>
2025-01-02 17:06:10 +0900

    Chore: 친구 entity 작성

--- src/main/java/com/harmony/entity/Friend.java
+++ src/main/java/com/harmony/entity/Friend.java
@@ -1,5 +1,47 @@
 package com.harmony.entity;
 
-public class Friend {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@ToString
+@Getter
+@Builder
+@Entity
+@Table(name = "friends")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Friend {
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  @Column(name="friend_id", nullable = false)
+  private Long friendId;
+
+  @ManyToOne
+  @JoinColumn(name="from_user_id")
+  private User fromUser;
+
+  @ManyToOne
+  @JoinColumn(name="to_user_id")
+  private User toUser;
+
+  @Column(name="is_friend", nullable=false)
+  Boolean isFriend;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name="friend_type", nullable = false)
+  FriendType friendType;
 }
---
+++ src/main/java/com/harmony/entity/FriendType.java
@@ -0,0 +1,5 @@
+package com.harmony.entity;
+
+public enum FriendType {
+  FRIEND, BEST_FRIEND
+}
