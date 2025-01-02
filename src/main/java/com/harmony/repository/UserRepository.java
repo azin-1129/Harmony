797318848354d40d4ed0f35e5b8e53431636b12c
azin1129 <nuestiloveyou@gmail.com>
2025-01-02 23:25:15 +0900

    Chore: Bean을 생성하는 annotation 작성

--- src/main/java/com/harmony/repository/UserRepository.java
+++ src/main/java/com/harmony/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.harmony.repository;
 
-public interface UserRepository {
+import com.harmony.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String mail);
 }
--- src/main/java/com/harmony/service/UserService.java
+++ src/main/java/com/harmony/service/UserService.java
@@ -1,5 +1,8 @@
 package com.harmony.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
 }
