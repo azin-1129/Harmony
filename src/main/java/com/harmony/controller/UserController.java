154db43c0bbb7eb1a15e3de92939f749d2b66de7
azin1129 <nuestiloveyou@gmail.com>
2025-01-07 23:28:17 +0900

    Feat: Dto 입력 형식 검증기능 추가

--- src/main/java/com/harmony/controller/UserController.java
+++ src/main/java/com/harmony/controller/UserController.java
@@ -4,12 +4,20 @@ import com.harmony.dto.form.NicknameForm;
 import com.harmony.dto.form.PasswordForm;
 import com.harmony.dto.form.ProfileImageForm;
 import com.harmony.dto.request.RegisterRequestDto;
+import com.harmony.global.response.code.ErrorCode;
 import com.harmony.global.response.code.SuccessCode;
+import com.harmony.global.response.structure.ErrorResponse;
 import com.harmony.global.response.structure.SuccessResponse;
 import com.harmony.service.UserService;
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +35,21 @@ public class UserController {
 
   // TODO: userId 처리, PasswordForm validation
   @PostMapping("/register")
-  public ResponseEntity<Object> registerUser(@RequestBody RegisterRequestDto registerRequestDto){
+  public ResponseEntity<Object> registerUser(@RequestBody @Valid RegisterRequestDto registerRequestDto, BindingResult bindingResult){
+    // 입력 형식이 올바르지 않다면 에러
+    if(bindingResult.hasErrors()){
+      Map<String, String> validatorResult=new HashMap<>();
+
+      for(FieldError error:bindingResult.getFieldErrors()){
+        String validKeyName=String.format("valid_%s",error.getField());
+        validatorResult.put(validKeyName, error.getDefaultMessage());
+      }
+
+      return ErrorResponse.createError(
+          ErrorCode.INVALID_ARGUMENT,
+          validatorResult
+      );
+    }
     userService.registerUser(registerRequestDto);
 
     return SuccessResponse.createSuccess(
--- src/main/java/com/harmony/dto/request/RegisterRequestDto.java
+++ src/main/java/com/harmony/dto/request/RegisterRequestDto.java
@@ -1,6 +1,9 @@
 package com.harmony.dto.request;
 
 import com.harmony.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Builder;
@@ -14,8 +17,23 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RegisterRequestDto {
+  // TODO: 정규식 수정
+  @NotBlank(message="이메일을 입력하세요.")
+  @Email
   private String email;
+
+  @NotBlank(message="식별자를 입력하세요.")
+  @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "식별자는 영문 소문자와 숫자 4~20자리여야 합니다.")
   private String userIdentifier;
+
+  // TODO: 닉네임 추가
+
+  @NotBlank(message="비밀번호를 입력하세요.")
+  @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+      message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8~20자의 비밀번호여야 합니다.")
   private String password;
+
+  // TODO: password Confirm
+
   private Role role;
 }
