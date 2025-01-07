c5df3aa4022f307adc5bbe4fed8009eb9e984d81
azin1129 <nuestiloveyou@gmail.com>
2025-01-07 23:28:42 +0900

    Fix: 회원가입 시 user 저장이 안되는 현상 해결

--- src/main/java/com/harmony/service/UserService.java
+++ src/main/java/com/harmony/service/UserService.java
@@ -10,34 +10,39 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UserService {
   private final UserRepository userRepository;
 
-  // TODO: 예외처리
+  // TODO: 예외처리, 닉네임 설정
   public void registerUser(RegisterRequestDto registerRequestDto) {
     User user= User.builder()
-            .build();
+      .email(registerRequestDto.getEmail())
+      .userIdentifier(registerRequestDto.getUserIdentifier())
+      .password(registerRequestDto.getPassword())
+      .profileImageName("")
+      .nickname("닉변요망")
+      .withdraw(false)
+      .role(registerRequestDto.getRole())
+      .build();
 
     userRepository.save(user);
   }
 
   // 비번 업뎃
-  @Transactional
   public void updateUserPassword(Long userId, String newPassword) {
     User user=userRepository.findById(userId).orElse(null);
     user.updatePassword(newPassword);
   }
 
   // 닉넴 업뎃
-  @Transactional
   public void updateUserNickname(Long userId, String newNickname) {
     User user=userRepository.findById(userId).orElse(null);
     user.updateNickname(newNickname);
   }
 
   // 프사 업뎃
-  @Transactional
   public void updateUserProfileImage(Long userId, String newProfileImageName) {
     User user=userRepository.findById(userId).orElse(null);
     user.updateProfileImage(newProfileImageName);
