431f3c19dc7007d1f269c3d7ab748bc69720b1a5
azin1129 <nuestiloveyou@gmail.com>
2025-01-09 17:19:29 +0900

    Chore: 닉네임, 비밀번호 확인 추가

--- src/main/java/com/harmony/dto/request/RegisterRequestDto.java
+++ src/main/java/com/harmony/dto/request/RegisterRequestDto.java
@@ -23,17 +23,20 @@ public class RegisterRequestDto {
   private String email;
 
   @NotBlank(message="식별자를 입력하세요.")
-  @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "식별자는 영문 소문자와 숫자 4~20자리여야 합니다.")
+  @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "식별자는 영문 소문자 또는 숫자 4~20자리여야 합니다.")
   private String userIdentifier;
 
-  // TODO: 닉네임 추가
+  @NotBlank(message="닉네임을 입력하세요.")
+  @Pattern(regexp="^[가-힣]{2,10}$", message="닉네임은 한글 2~10자리여야 합니다.")
+  private String nickname;
 
   @NotBlank(message="비밀번호를 입력하세요.")
   @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
       message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8~20자의 비밀번호여야 합니다.")
   private String password;
 
-  // TODO: password Confirm
+  @NotBlank(message="비밀번호 확인을 입력하세요.")
+  private String passwordConfirm;
 
   private Role role;
 }
