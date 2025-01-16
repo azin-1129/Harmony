945d2a9d5a1af067135e6e59319ce171bdb86c16
azin1129 <nuestiloveyou@gmail.com>
2025-01-16 20:37:00 +0900

    Remove: 채팅방 생성 기능을 Participant 쪽으로 이관

--- src/main/java/com/harmony/controller/ChatRoomController.java
+++ src/main/java/com/harmony/controller/ChatRoomController.java
@@ -1,8 +1,6 @@
 package com.harmony.controller;
 
 import com.harmony.dto.form.ChatRoomNameForm;
-import com.harmony.dto.request.GroupChatRoomCreateRequestDto;
-import com.harmony.dto.request.PersonalChatRoomCreateRequestDto;
 import com.harmony.global.response.code.ErrorCode;
 import com.harmony.global.response.code.SuccessCode;
 import com.harmony.global.response.structure.ErrorResponse;
@@ -35,55 +33,9 @@ public class ChatRoomController {
 
   // TODO: bindingResult 메소드 추가?
 
-  // 채팅방 생성
-  // 개인
-  @PostMapping("/create/personal")
-  @Operation(summary="개인 채팅방 생성", description="개인 채팅방 생성 API")
-  @ApiResponses(value={
-      @ApiResponse(responseCode="201", description="개인 채팅방 생성 성공")
-  })
-  public ResponseEntity<Object> createPersonalChatRoom(@RequestBody
-      PersonalChatRoomCreateRequestDto personalChatRoomCreateRequestDto) {
-    chatRoomService.createPersonalChatRoom(personalChatRoomCreateRequestDto);
-
-    return SuccessResponse.createSuccess(
-        SuccessCode.PERSONAL_CHATROOM_CREATE_SUCCESS
-    );
-  }
-
-  // 단체
-  @PostMapping("/create/group")
-  @Operation(summary="단체 채팅방 생성", description="단체 채팅방 생성 API")
-  @ApiResponses(value={
-      @ApiResponse(responseCode="201", description="단체 채팅방 생성 성공")
-  })
-  public ResponseEntity<Object> createGroupChatRoom(@RequestBody @Valid
-  GroupChatRoomCreateRequestDto groupChatRoomCreateRequestDto, BindingResult bindingResult) {
-    // 입력 형식이 올바르지 않다면 에러
-    if(bindingResult.hasErrors()){
-      Map<String, String> validatorResult=new HashMap<>();
-
-      for(FieldError error:bindingResult.getFieldErrors()){
-        String validKeyName=String.format("valid_%s",error.getField());
-        validatorResult.put(validKeyName, error.getDefaultMessage());
-      }
-
-      return ErrorResponse.createError(
-          ErrorCode.INVALID_ARGUMENT,
-          validatorResult
-      );
-    }
-
-    chatRoomService.createGroupChatRoom(groupChatRoomCreateRequestDto);
-
-    return SuccessResponse.createSuccess(
-        SuccessCode.GROUP_CHATROOM_CREATE_SUCCESS
-    );
-  }
-
   // 채팅방 조회(보류)
 
-  // 채팅방 변경
+  // 변경
   // 채팅방 이름
   @PostMapping("/update")
   @Operation(summary="채팅방 이름 변경", description="채팅방 이름 변경 API")
