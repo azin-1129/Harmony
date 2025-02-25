d12fa9837aa38016735f162973e201db685e653e
azin1129 <nuestiloveyou@gmail.com>
2025-02-25 15:06:58 +0900

    Feat: 친구 service 작성

---
+++ src/main/java/com/harmony/dto/response/FriendshipResponseDto.java
@@ -0,0 +1,21 @@
+package com.harmony.dto.response;
+
+import com.harmony.entity.FriendType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FriendshipResponseDto {
+  private String friendIdentifier;
+  private String nickname;
+  private String profileImageName;
+  private FriendType friendType;
+}
--- src/main/java/com/harmony/service/FriendshipService.java
+++ src/main/java/com/harmony/service/FriendshipService.java
@@ -1,5 +1,60 @@
 package com.harmony.service;
 
-public class FriendshipService {
+import com.harmony.dto.response.FriendshipResponseDto;
+import com.harmony.entity.Friendship;
+import com.harmony.entity.User;
+import com.harmony.global.response.code.ErrorCode;
+import com.harmony.global.response.exception.EntityNotFoundException;
+import com.harmony.repository.FriendshipRepository;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class FriendshipService {
+  private final FriendshipRepository friendshipRepository;
+  private final UserService userService;
+
+  public List<FriendshipResponseDto> readFriendships(Long userId){
+    List<Friendship> friendships=friendshipRepository.findByUserId(userId);
+
+    List<FriendshipResponseDto> friendshipResponseDtos=new ArrayList<>();
+    for(Friendship friendship:friendships){
+      FriendshipResponseDto friendshipResponseDto= FriendshipResponseDto.builder()
+          .friendIdentifier(friendship.getFriend().getUserIdentifier())
+          .nickname(friendship.getFriend().getNickname())
+          .profileImageName(friendship.getFriend().getProfileImageName())
+          .friendType(friendship.getFriendType())
+          .build();
+      friendshipResponseDtos.add(friendshipResponseDto);
+    }
+
+    return friendshipResponseDtos;
+  }
+
+  public void deleteFriendship(Long userId, String friendIdentifier){
+    User user=userService.getUserById(userId);
+    User friend=userService.getUserByUserIdentifier(friendIdentifier);
+
+    Optional<Friendship> friendship=friendshipRepository.findByUserIdAndFriendId(userId, friend.getUserId());
+    if(friendship.isPresent()){
+      user.getFriendships().remove(friendship.get());
+
+      Optional<Friendship> reversedFriendship=friendshipRepository.findByUserIdAndFriendId(friend.getUserId(), userId);
+      friend.getFriendships().remove(reversedFriendship.get());
+
+      System.out.println("친구를 삭제했습니다.");
+    }else{
+      throw new EntityNotFoundException(
+          ErrorCode.USER_NOT_FOUND
+      );
+    }
+  }
 }
