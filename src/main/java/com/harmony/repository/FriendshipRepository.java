cccd959d6dbb45aad76ba9f3addc3b6bf322c7e5
azin1129 <nuestiloveyou@gmail.com>
2025-02-25 15:06:48 +0900

    Feat: 친구 repository 작성

--- src/main/java/com/harmony/repository/FriendshipRepository.java
+++ src/main/java/com/harmony/repository/FriendshipRepository.java
@@ -1,5 +1,19 @@
 package com.harmony.repository;
 
-public interface FriendshipRepository {
+import com.harmony.entity.Friendship;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+  @Query("SELECT f FROM Friendship f WHERE f.user.userId = :userId")
+  List<Friendship> findByUserId(@Param("userId") Long userId);
+
+  @Query("SELECT f FROM Friendship f WHERE f.user.userId = :userId AND f.friend.userId = :friendId")
+  Optional<Friendship> findByUserIdAndFriendId(@Param("userId") Long userId, @Param("friendId") Long friendId);
 
 }
