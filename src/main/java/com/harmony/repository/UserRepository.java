6dd720589061118bbd915ca6c75a719f1459f896
azin1129 <nuestiloveyou@gmail.com>
2025-05-02 14:22:52 +0900

    Chore: 메소드 파라미터 이름 직관적으로 변경

--- src/main/java/com/harmony/repository/UserRepository.java
+++ src/main/java/com/harmony/repository/UserRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Optional<User> findByEmail(String mail);
+  Optional<User> findByEmail(String email);
   Optional<User> findByUserIdentifier(String userIdentifier);
   Optional<User> findByNickname(String nickname);
 }
